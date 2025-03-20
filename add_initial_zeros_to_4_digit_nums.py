aab6711e29268e8216fafd7e330db431ae5558b6
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-20 20:28:24 +0800

    Print output

--- add_initial_zeros_to_4_digit_nums.py
+++ add_initial_zeros_to_4_digit_nums.py
@@ -1,6 +1,8 @@
 # ask user to input a number
 
-num = int(input("Please enter a number"))
+num = int(input("Please enter a number between 1-1000s: "))
 
 # format the number
+formatted_num = str(num).zfill(6)
 # print output
+print(formatted_num)
