1ebf35c4f3a8c487e82bad69ccc345ce45b38212
Friedrice-1 <allenjayansing99@gmail.com>
2025-03-20 21:34:50 +0800

    Print result

--- name_character_counter.py
+++ name_character_counter.py
@@ -3,4 +3,6 @@
 name = input("Please input your name: ")
 
 # count characters in the name including spaces
+char_count = len(name)
 # print result
+print(f"There are {char_count} characters in your name, including spaces.")
